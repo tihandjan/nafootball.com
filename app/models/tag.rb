ac409c64419a4f023a851b27ac79eb3627976d02
tihandjan <tihandjan198921@gmail.com>
2016-12-27 03:37:26 +0300

    change my_team

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ActiveRecord::Base
     has_many :taggings
     has_many :articles, through: :taggings
     has_many :videos, through: :taggings
-    validates :name, uniqueness: true
+    validates :name, presence: true, uniqueness: true
 
     def to_param
     "#{id} #{name}".parameterize
