3ec5a7eae71e57cadc1cba238b50f88f113d2a06
tihandjan <tihandjan198921@gmail.com>
2017-01-03 06:34:06 +0300

    create additional menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :configure_permitted_parameters_for_edit, if: :devise_controller?
+  before_action :set_up_teams
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -22,6 +23,10 @@ class ApplicationController < ActionController::Base
       format.html { redirect_to main_app.root_url, alert: exception.message }
       format.js   { head :forbidden, content_type: 'text/html' }
     end
+  end
+
+  def set_up_teams
+    @teams_all ||= Team.all
   end
 
 end
